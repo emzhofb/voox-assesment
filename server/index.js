94c99d9c90d6f7200160b92e6ac8ee5ea420d102
ikhdamhc <ikhdawildani@mhcasia.com>
2022-01-24 15:13:18 +0700

    finishing

--- server/index.js
+++ server/index.js
@@ -2,17 +2,42 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
 
 const readJSON = fs.readFileSync('./data.json');
 let data = JSON.parse(readJSON);
 
 app.get('/', (req, res, next) => {
+  const { priority, label } = req.query;
+  let returnedData = [...data];
+  
+  if (priority) {
+    returnedData = returnedData.filter((v, i) => {
+      return v.priority === priority.toLocaleLowerCase();
+    });  
+  }
+  if (label) {
+    returnedData = returnedData.filter((v, i) => {
+      return v.label === label.toLocaleLowerCase();
+    });  
+  }
+  
   res.status(200).json({
-    data: data
+    data: returnedData
   });
 });
 
